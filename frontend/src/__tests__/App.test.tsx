3f7daa17c86ef894acf807ee1b036fecc7a2443f
Denislav Simeonov -X (desimeon - SOFTSERVE INC at Cisco) <desimeon@cisco.com>
2023-03-18 15:56:23 +0200

    Add ESLint And Prettier

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,9 +1,9 @@
-import { render } from "global/test-utils";
-import App from "../App";
+import { render } from 'global/test-utils';
+import App from '../App';
 
-describe("App Components", () => {
-  it("should render correct content", () => {
+describe('App Components', () => {
+  it('should render correct content', () => {
     const { container } = render(<App />);
-    expect(container).toHaveTextContent("App");
+    expect(container).toHaveTextContent('App');
   });
 });
--- frontend/src/global/test-utils.tsx
+++ frontend/src/global/test-utils.tsx
@@ -1,15 +1,13 @@
-import React, { ReactElement } from "react";
-import { render, RenderOptions } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import React, { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
 
-const customRender = (
-  ui: ReactElement,
-  options?: Omit<RenderOptions, "wrapper">
-) => render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
 
-export * from "@testing-library/react";
+export * from '@testing-library/react';
 export { customRender as render };
